16752860909daae9eb1ba00a088a5ebb64bfdee3
alexa <alexa@LAPTOP-U6ESD9BJ>
2021-10-01 15:37:19 +0100

    Adicionar arquivos de projeto.

---
+++ formulário1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formulário1
+{
+    public partial class Form1 : Form
+    {
+        private string str;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox3.Text);
+            listBox1.Items.Add(DateTime.Now.ToString("hh:mm:ss tt"));
+            listBox1.Items.Add(DateTime.Now.ToString("dd MMMM,yyyy"));
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+
+
+        }
+    }
+}
